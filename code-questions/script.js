0950763a8fa99ed3fd6cde0d29167d13d991976e
Francesca-Lisi <francescalisi2143@gmail.com>
2025-03-31 12:59:53 +0200

    Feat: questions 6 and snack completed, errors fixed

--- code-questions/script.js
+++ code-questions/script.js
@@ -11,7 +11,7 @@
 
 
 //********* RISPOSTA
-//In console verranno stampati rispettivamente 'Double Cheese Burger' e 500.
+//In console verranno stampati rispettivamente 'Double Cheese Burger' e 'Double Cheese Burger'.
 //L'oggetto all'interno di questo codice è solo 1, in quanto le variabili dichiarate non contengono l'oggetto stesso ma solo il suo riferimento.
 
 
@@ -37,8 +37,8 @@
 
 
 //********* RISPOSTA
-//In console verranno stampati rispettivamente "Cheese" e "Salad".
-//L'oggetto all'interno di questo codice sono 2, in quanto con l'operatore spread a riga 29 del codice abbiamo creato una copia dell'oggetto 'hamburger'.
+//In console verranno stampati rispettivamente "Salad" e "Salad".
+//L'oggetto all'interno di questo codice sono 3, in quanto con l'operatore spread a riga 29 del codice abbiamo creato una copia dell'oggetto 'hamburger' e l'array degli ingrdients figura come terzo oggetto .
 
 
 
@@ -62,8 +62,7 @@
 // Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
 
 //********* RISPOSTA
-// durante l'esecuzione di questo codice otterremo 3 oggetti (rispettivamente hamburger, secondBurger e thirdBurger) grazie all'uso di structuredClone(), che ci permette di fare una copia di oggetti complessi.
-
+// durante l'esecuzione di questo codice otterremo 9 oggetti (rispettivamente hamburger, secondBurger e thirdBurger con i loro oggetti annidati) .
 
 
 // 🏆 Code Question 4
@@ -116,45 +115,56 @@
 // secondBurger.maker.restaurant = newRestaurant;
 // secondBurger.maker.name = "Chef Hyur";
 //
-// console.log(hamburger.maker.name); // ?             "Anonymous Chef"
-// console.log(secondBurger.maker.name); // ?          "Hyur's II"
-// console.log(hamburger.maker.restaurant.name); // ?   "Hyur's Burgers"
-// console.log(secondBurger.maker.restaurant.name); // ?  "Chef Hyur"
+// console.log(hamburger.maker.name); // ?             "Chef Hyur"
+// console.log(secondBurger.maker.name); // ?          "Chef Hyur"
+// console.log(hamburger.maker.restaurant.name); // ?   "Hyur's II"
+// console.log(secondBurger.maker.restaurant.name); // ?  "Hyur's II"
 // Senza lanciare il codice, riesci a prevedere cosa viene stampato in console?
 
 // Quanti oggetti sono stati creati in memoria durante l'esecuzione di questo codice?
 //********* RISPOSTA
-// Durante l'esecuzione di questo codice otterremo 3 oggetti, rispettivamente hamburger, newRestaurant e secondBurger.
+// Durante l'esecuzione di questo codice otterremo 5 oggetti, rispettivamente hamburger, maker, restaurant, newRestaurant e secondBurger.
 
 
 
 
 // 🎯 Code Question 6 (Bonus)
-// const chef = {
-//     name: "Chef Hyur",
-//     age: 29,
-//     makeBurger: (num = 1) => {
-//         console.log(`Ecco ${num} hamburger per te!`);
-//     },
-//     restaurant: {
-//         name: "Hyur's Burgers",
-//         welcomeClient: () => {
-//             console.log("Benvenuto!");
-//         },
-//         address: {
-//             street: 'Main Street',
-//             number: 123,
-//             showAddress: () => {
-//                 console.log("Main Street 123");
-//             }
-//         },
-//         isOpen: true,
-//     }
-// }
+const chef = {
+    name: "Chef Hyur",
+    age: 29,
+    makeBurger: (num = 1) => {
+        console.log(`Ecco ${num} hamburger per te!`);
+    },
+    restaurant: {
+        name: "Hyur's Burgers",
+        welcomeClient: () => {
+            console.log("Benvenuto!");
+        },
+        address: {
+            street: 'Main Street',
+            number: 123,
+            list: [1, 2, 3],
+            showAddress: () => {
+                console.log("Main Street 123");
+                isOpen: true
+            }
+        },
+    }
+}
 // Qual è il metodo migliore per clonare l’oggetto chef, e perché?
 
 //********* RISPOSTA
-//
+// Dal momento che non esiste un metodo diretto che mi permette di copiare un oggetto che presenta oggetti annidati con funzioni al loro interno bisogna utilizzare più operatori Spread annidati tra loro:
+
+// const chefCopy ={
+//     ...chef,
+//     restaurant: {
+//         ...chef.restaurant,
+//         address: {
+//             ...chef.restaurant.address
+//         }
+//     }
+// }
 
 
 
@@ -162,3 +172,25 @@
 
 // 🎯 Snack  (Bonus)
 // Crea una funzione che permette la copia profonda (deep copy) di un oggetto, che copia anche i suoi metodi (proprietà che contengono funzioni). Usa l’oggetto di Code Question 6 come test.
+
+function deepCopy(oggetto) {
+    if (typeof oggetto !== 'object') {
+        return oggetto
+    }
+    const copia = {}
+    for (const key in oggetto) {
+        const valore = oggetto[key]
+        if (typeof valore !== 'object') {
+            copia[key] = valore
+        } else if (typeof valore === 'object' && Array.isArray(valore)) {
+            copia[key] = valore
+        }
+        else {
+            copia[key] = deepCopy(valore)
+        }
+    }
+    return copia
+}
+
+const chefCopia = deepCopy(chef)
+console.log(chefCopia)
